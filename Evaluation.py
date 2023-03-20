7d6fff98ac3b57841d71b3d45923dc3b9e988afe
GabrieleLerani <gabriele.lerani2000@gmail.com>
2023-03-21 00:04:51 +0100

    Evaluation,simple modification

--- Evaluation.py
+++ Evaluation.py
@@ -1,19 +1,21 @@
 import time
 import tracemalloc
 
+
 class Evaluation:
     def __init__(self):
         self.start = 0
         self.end = 0
         self.snap = []
+        self.execution_time = 0
 
     def check_time(self):
         if self.start != 0:
             self.end = time.time()
-            temp = self.end - self.start
+            self.execution_time = self.end - self.start
             self.start = 0
             self.end = 0
-            print(temp)
+            print(f"Execution time: {self.execution_time} ms")
         self.start = time.time()
 
     def start_check_resources(self):
@@ -23,7 +25,9 @@ class Evaluation:
         self.snap.append([tracemalloc.take_snapshot(), flag])
 
     def snap_diff(self):
-        compare = self.snap[len(self.snap) - 1].compare_to(self.snap[len(self.snap) - 2], 'lineno', False)
+        compare = self.snap[len(self.snap) - 1].compare_to(
+            self.snap[len(self.snap) - 2], "lineno", False
+        )
         for i in compare:
             print(i)
 
@@ -34,7 +38,11 @@ class Evaluation:
 
     def memory_used(self):
         size, peak = tracemalloc.get_tracemalloc_memory()
-        print("Size is: " + size + "\nPeak is: " + peak)
+        print("Size is:", size, "\nPeak is:", peak)
 
     def stop_check_resources(self):
         tracemalloc.stop()
+
+    # TODO
+    def set_execution_time(self, exec_time):
+        self.execution_time = exec_time
