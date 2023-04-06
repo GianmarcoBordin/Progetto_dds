342d12e04cffd047d51d082742eaa8fa77efedd8
CarloGiralda <92364122+CarloGiralda@users.noreply.github.com>
2023-04-06 12:27:32 +0200

    Add files via upload

--- AuthenticatedLink.py
+++ AuthenticatedLink.py
@@ -2,23 +2,24 @@ import socket
 import hashlib
 import hmac
 import json
-import logging
+import pika
 from threading import Thread
+import threading
 
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 RCV_BUFFER_SIZE = 1024
 KEY_SIZE = 32
-
+SERVER_IP = "192.168.1.41"
 
 class AuthenticatedLink:
     def __init__(self, self_id, self_ip, idn, ip, proc):
         self.proc = proc
-        self.self_id = self_id     # id of the process that is creating this instance
-        self.id = idn              # id of the other process
-        self.self_ip = self_ip     # ip of the process that is creating this instance
-        self.ip = ip               # ip of the other process
-        self.key = {}              # key exchanged between the two processes
+        self.self_id = self_id  # id of sending process
+        self.id = idn  # id of receiving process
+        self.self_ip = self_ip
+        self.ip = ip
+        self.key = {}
 
     def get_id(self):
         return self.self_id
@@ -29,148 +30,108 @@ class AuthenticatedLink:
         t.start()
 
     # This handles the message receive
-    # Now the listening port is the concatenation 50/5 - 'receiving process' - 'sending process'
     def __receive(self):
-        ready = False
-        host = ""  # Symbolic name meaning all available interfaces
-        # It uses ternary operator
-        port = (
-            int("50" + str(self.id) + str(self.self_id))
-            if self.self_id < 10 and self.id < 10
-            else int("5" + str(self.id) + str(self.self_id))
-        )
+        # The queue name is the concatenation of the sender id and the receiver id
+        queue_id = str(self.id) + str(self.self_id)
 
-        print(port)   # useful to check which connection is being created
+        with pika.BlockingConnection(pika.ConnectionParameters(host=SERVER_IP)) as connection:
+            channel = connection.channel()
+            channel.queue_declare(queue=queue_id)
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
-            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.s.bind((host, port))
-            self.s.listen(0)
-            while True:
-                conn, addr = self.s.accept()
+            def callback(ch, method, properties, body):
+                queue_msg = body.decode("utf-8")
 
-                with conn:
-                    while True:
-                        data = conn.recv(RCV_BUFFER_SIZE)
-                        if not data:
-                            break
+                print("Message received by ", self.ip, ":", queue_msg)
 
-                        parsed_data = json.loads(data.decode())
+                if "Flag" not in queue_msg.keys():
+                    self.__add_key(queue_msg)
+                else:
+                    t = Thread(
+                        target=self.__receiving,
+                        args=(queue_msg,),
+                    )
+                    t.start()
+                    # if you receive an ACC for some message M from some other process,
+                    # it means that it received at least n-f ECHOs for that message M,
+                    # so it is safe to close the socket with it
+                    # (it received at least f+1 ECHOs from correct processes,
+                    # so it is impossible that it will send a REQ message;
+                    # in fact, even if it receives the same message from all the faulty processes
+                    # it will not send it because they are at most f)
+                    # Otherwise, if you don't receive an ACC from someone,
+                    # it may mean that it did not receive the message at all,
+                    # so it may ask you about the message associated to the ACC that it received
+                    # (indeed, you will send / sent an ACC message to it too)
+                    if "ACC" in queue_msg.values():
+                        channel.stop_consuming()
+                        channel.close()
 
-                        print("Message received by ", self.ip, ":", parsed_data)
-
-                        if "Flag" not in parsed_data.keys():
-                            self.__add_key(parsed_data)
-                            conn.sendall(b"synACK")
-                        else:
-                            t = Thread(
-                                target=self.__receiving,
-                                args=(parsed_data,),
-                            )
-                            t.start()
-                            # if you receive an ACC for some message M from some other process,
-                            # it means that it received at least n-f ECHOs for that message M,
-                            # so it is safe to close the socket with it
-                            # (it received at least f+1 ECHOs from correct processes,
-                            # so it is impossible that it will send a REQ message;
-                            # in fact, even if it receives the same message from all the faulty processes
-                            # it will not send it because they are at most f)
-                            # Otherwise, if you don't receive an ACC from someone,
-                            # it may mean that it did not receive the message at all,
-                            # so it may ask you about the message associated to the ACC that it received
-                            # (indeed, you will send / sent an ACC message to it too)
-                            if "ACC" in parsed_data.values():
-                                ready = True
-
-                if ready:
-                    break
+            channel.basic_consume(
+                queue=queue_id, on_message_callback=callback, auto_ack=True
+            )
+            channel.start_consuming()
 
     def __add_key(self, key_dict):
         self.key[self.id] = key_dict["KEY"].encode("latin1")
 
-        logging.info(
-            "AUTH: <%s, %d> is the one with this key: %s", self.ip, self.id, self.key
-        )
-
     def __check(self, idn):
         if idn not in self.key:
             self.key[idn] = ChaCha20Poly1305.generate_key()
-
             key_to_send = {"KEY": self.key[idn].decode("latin1")}
-            logging.info("AUTH: Key generated")
+            # The queue name is the concatenation of the sender id and the receiver id
+            # Same as before but they are in reverse order because now this process is the sender
+            queue_id = str(self.self_id) + str(self.id)
 
-            data = json.dumps(key_to_send)
-            self.sock.sendall(data.encode())
-            self.temp = self.sock.recv(RCV_BUFFER_SIZE, 0).decode()
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=SERVER_IP))  # Connect to CloudAMQP
+            channel = connection.channel()  # start a channel
+            channel.queue_declare(queue=queue_id)  # naming queue
+            channel.basic_publish(exchange='', routing_key=queue_id,
+                                  body=bytes(str(key_to_send), "utf-8"))
+            connection.close()  # closing connection
 
-            if self.temp != "synACK":  # Ack used for synchronization with other process
-                return 1
-
-    # Compute the hmac of the message with the key exchanged
-    # The message is returned as a dictionary: {"FLAG": flag, "MSG": message, "HMAC": hmac}
-    # The hmac is computed starting from the concatenation of all the fields in the message
+    # Compute the hmac of the message with a key associated to the process with self.id
+    # The message is returned as a dictionary: {"MSG": message,"HMAC": hmac, "FLAG": flag}
+    # The hmac is computed starting from the concatenation of flag and message
+    # Example: flag = "SEND" , message = "Hello" ----> HMAC("SENDHello")
     def __auth(self, message):
         self.__check(self.id)
-        # This creates the string that will be authenticated
-        hmac_input = ""
-        for value in message.values():
-            hmac_input += str(value)
-        # This creates the message that will be sent
-        mess = {}
-        dict_hmac = {"HMAC": hmac.new(
-                    self.key.get(self.id, "Key not found"), hmac_input.encode("utf-8"), hashlib.sha256,).hexdigest(),
-                     }
-        for key in message.keys():
-            dict_temp = {key: message[key]}   # Adding to the dictionary all the field that
-            mess.update(dict_temp)            # were inside the original message
-        mess.update(dict_hmac)                # Adding to it the HMAC just computed
+        mess = {
+            "Flag": message["Flag"], "Source": message["Source"], "Message": message["Message"],
+            "SequenceNumber": message["SequenceNumber"], "HMAC": hmac.new(
+                self.key.get(self.id, "Key not found"),
+                (message["Flag"] + str(message["Source"]) + message["Message"] + str(message["SequenceNumber"]))
+                .encode("utf-8"), hashlib.sha256,
+            ).hexdigest(),
+        }
         return mess
 
-    # The SEND opens a new socket, the port is the concatenation of 50/5-
-    # id of sending process - id of receiving process
-    # Example: sending_id = 1, receiving_id = 2 ---> port = 5012
     def send(self, message):
-        # It uses ternary operator
-        port = (
-            int("50" + str(self.self_id) + str(self.id))
-            if self.self_id < 10 and self.id < 10
-            else int("5" + str(self.self_id) + str(self.id))
-        )
-
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.sock:
-            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            while True:
-                # Try Except used to repeat the connection until the other socket is opened again
-                try:
-                    self.sock.connect((self.ip, port))
-
-                    # mess is a dictionary that contains the original packet plus the HMAC
-                    mess = self.__auth(message)
-
-                    print(mess, "sent to <", self.ip, self.id, ">")
-
-                    parsed_data = json.dumps(mess)
-                    self.sock.sendall(bytes(parsed_data, encoding="utf-8"))
-                    break
-                except ConnectionRefusedError:
-                    continue
+        # The queue name is the concatenation of the sender id and the receiver id
+        # Same as before but they are in reverse order because now this process is the sender
+        queue_id = str(self.self_id) + str(self.id)
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=SERVER_IP))  # Connect to CloudAMQP
+        channel = connection.channel()  # start a channel
+        channel.queue_declare(queue=queue_id)  # naming queue
+        channel.basic_publish(exchange='', routing_key=queue_id,
+                              body=bytes(str(message), "utf-8"))
+        connection.close()  # closing connection
 
     # It checks message authenticity comparing the hmac
     def __check_auth(self, message):
-        # This creates the string that should match with the HMAC
-        hmac_input = ""
-        for value in message.values():
-            hmac_input += str(value)
-        temp_hash = hmac.new(self.key.get(self.id, "Key not found"), hmac_input.encode("utf-8"), hashlib.sha256,)\
-            .hexdigest()
-        # The HMAC field is always present in the Authenticated Link implementation
+        temp_hash = hmac.new(
+            self.key.get(self.id, "Key not found"),
+            (message["Flag"] + str(message["Source"]) + message["Message"] + str(message["SequenceNumber"])).encode("utf-8"),
+            hashlib.sha256,
+        ).hexdigest()
         return temp_hash == message["HMAC"]
 
     def __receiving(self, message):
         flag = message["Flag"]
 
         if not self.__check_auth(message):
-            logging.info("--- Authenticity check failed for %s", message)
+            pass
             # TODO what do if authenticity check fails??
 
         # this is done in order to pass to the upper layer only the part that it requires
